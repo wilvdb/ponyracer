c99a18bd0610164bd7354713faefadbbc089a3eb
obionewilobi <wilfried.vandenberghe@gmail.com>
2017-06-05 21:32:46 +0200

    Update specs

--- src/app/races/races.component.spec.ts
+++ src/app/races/races.component.spec.ts
@@ -1,25 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
+import { AppModule } from '../app.module';
 import { RacesComponent } from './races.component';
 
 describe('RacesComponent', () => {
-  let component: RacesComponent;
-  let fixture: ComponentFixture<RacesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RacesComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [AppModule]
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RacesComponent);
-    component = fixture.componentInstance;
+  it('should display every race name in a title', () => {
+    const fixture = TestBed.createComponent(RacesComponent);
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance.races).not.toBeNull('You need to have a field `races` initialized with 2 races');
+    expect(fixture.componentInstance.races.length).toBe(2, 'You need to have a field `races` initialized with 2 races');
+    expect(fixture.componentInstance.races[0].name).toBe('Lyon');
+    expect(fixture.componentInstance.races[1].name).toBe('London');
+
+    const element = fixture.nativeElement;
+    const raceNames = element.querySelectorAll('h2');
+    expect(raceNames.length).toBe(2, 'You should have an `h2` element per race in your template');
+    expect(raceNames[0].textContent).toContain('Lyon');
+    expect(raceNames[1].textContent).toContain('London');
   });
 });
