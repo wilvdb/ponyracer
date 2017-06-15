564d111d6231036cc5197bf24249e520b1cc2d7d
obionewilobi <wilfried.vandenberghe@gmail.com>
2017-06-15 21:47:30 +0200

    Update for exercise.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
-import { RacesComponent } from './races/races.component';
-import { By } from '@angular/platform-browser';
+import { AppModule } from '../app.module';
+import { RacesComponent } from './races.component';
+import { RaceService } from '../race.service';
 
-describe('AppComponent', () => {
+describe('RacesComponent', () => {
+
+  const service = jasmine.createSpyObj('RaceService', ['list']);
 
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppModule]
+    imports: [AppModule],
+    providers: [{ provide: RaceService, useValue: service }]
   }));
 
-  it('should have a title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const element = fixture.nativeElement;
-    expect(element.querySelector('h1').textContent).toContain('Ponyracer');
-  });
+  it('should display every race name in a title', () => {
+    service.list.and.returnValue(Observable.of([
+      {name: 'Lyon'},
+      {name: 'Los Angeles'},
+      {name: 'Sydney'},
+      {name: 'Tokyo'},
+      {name: 'Casablanca'}
+    ]));
 
-  it('should use the races component', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(RacesComponent);
     fixture.detectChanges();
-    const element = fixture.debugElement;
-    expect(element.query(By.directive(RacesComponent)))
-      .toBeDefined('You probably forgot to add RacesComponent to the AppComponent template');
+
+    expect(service.list).toHaveBeenCalled();
+
+    expect(fixture.componentInstance.races).not.toBeNull('You need to have a field `races` initialized with 5 races');
+    expect(fixture.componentInstance.races.length).toBe(5, 'You need to have a field `races` initialized with 5 races');
+    expect(fixture.componentInstance.races[0].name).toBe('Lyon');
+    expect(fixture.componentInstance.races[1].name).toBe('Los Angeles');
+    expect(fixture.componentInstance.races[2].name).toBe('Sydney');
+    expect(fixture.componentInstance.races[3].name).toBe('Tokyo');
+    expect(fixture.componentInstance.races[4].name).toBe('Casablanca');
+
+    const element = fixture.nativeElement;
+    const raceNames = element.querySelectorAll('h2');
+    expect(raceNames.length).toBe(4, 'You should have four `h2` elements, use the `slice` pipe');
+    expect(raceNames[0].textContent).toContain('Lyon');
+    expect(raceNames[1].textContent).toContain('Los Angeles');
+    expect(raceNames[2].textContent).toContain('Sydney');
+    expect(raceNames[3].textContent).toContain('Tokyo');
   });
 });
