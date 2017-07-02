4cad5a8d184a6f98ea17063300e63440fb41078c
obionewilobi <wilfried.vandenberghe@gmail.com>
2017-07-02 22:51:05 +0200

    Update exercise

--- src/app/race.service.spec.ts
+++ src/app/race.service.spec.ts
@@ -1,25 +1,48 @@
 import { async, TestBed } from '@angular/core/testing';
+import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import 'rxjs/add/observable/of';
 
 import { RaceService } from './race.service';
 import { RaceModel } from './models/race.model';
 
 describe('RaceService', () => {
 
-  let service: RaceService;
+  let raceService: RaceService;
+  let mockBackend: MockBackend;
 
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [RaceService]
+    providers: [
+      MockBackend,
+      BaseRequestOptions,
+      {
+        provide: Http,
+        useFactory: (backend, defaultOptions) => new Http(backend, defaultOptions),
+        deps: [MockBackend, BaseRequestOptions]
+      },
+      RaceService
+    ]
   }));
 
-  beforeEach(() => service = TestBed.get(RaceService));
+  beforeEach(() => {
+    raceService = TestBed.get(RaceService);
+    mockBackend = TestBed.get(MockBackend);
+  });
 
-  it('should list races', async(() => {
-    const observable = service.list();
-    observable.subscribe((races: Array<RaceModel>) => {
-      expect(races.length).toBe(2, 'The service should return two races');
-      const paris = races[0];
-      expect(paris.name).toBe('Paris');
-      expect(paris.ponies.length).toBe(5, 'The races should include the ponies');
+  it('should return an Observable of 3 races', async(() => {
+    // fake response
+    const hardcodedRaces = [{ name: 'Paris' }, { name: 'Tokyo' }, { name: 'Lyon' }];
+    const response = new Response(new ResponseOptions({ body: hardcodedRaces }));
+    // return the response if we have a connection to the MockBackend
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      expect(connection.request.url)
+        .toBe('http://ponyracer.ninja-squad.com/api/races?status=PENDING', 'The URL requested is not correct');
+      expect(connection.request.method).toBe(RequestMethod.Get);
+      connection.mockRespond(response);
+    });
+
+    raceService.list().subscribe((races: Array<RaceModel>) => {
+      expect(races.length).toBe(3, 'The `list` method should return an array of RaceModel wrapped in an Observable');
     });
   }));
 
