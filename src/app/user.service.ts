a98f877b74b74d61e229671efbec1589d6f32deb
obionewilobi <wilfried.vandenberghe@gmail.com>
2017-10-31 21:30:12 +0100

    solve exercise

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -12,6 +12,7 @@ export class UserService {
   public userEvents = new BehaviorSubject<UserModel>(undefined);
 
   constructor(private http: HttpClient) {
+    this.retrieveUser();
   }
 
   register(login, password, birthYear): Observable<UserModel> {
@@ -21,7 +22,20 @@ export class UserService {
 
   authenticate(credentials): Observable<UserModel> {
     return this.http.post<UserModel>('http://ponyracer.ninja-squad.com/api/users/authentication', credentials)
-      .do((user: UserModel) => this.userEvents.next(user));
+      .do((user: UserModel) => this.storeLoggedInUser(user));
+  }
+
+  storeLoggedInUser(user) {
+    window.localStorage.setItem('rememberMe', JSON.stringify(user));
+    this.userEvents.next(user);
+  }
+
+  retrieveUser() {
+    const strUser = window.localStorage.getItem('rememberMe');
+    if (strUser) {
+      const user = JSON.parse(strUser);
+      this.userEvents.next(user);
+    }
   }
 
 }
